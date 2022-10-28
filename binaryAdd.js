e8e0781feca247ad84a179761a3eda67fed3dbfb
Daniel Cha <cha.danieldev@gmail.com>
2022-10-27 23:53:42 -0400

    commit

--- binaryAdd.js
+++ binaryAdd.js
@@ -4,8 +4,8 @@
 
 // Examples:(Input1, Input2 --> Output (explanation)))
 
-1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
-5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
 function addBinary(a,b) {
     return parseInt(a + b).toString(2)
